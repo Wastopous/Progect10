7e16b3a1e28e3da57a98b0ceb49a54c67f2f4b9b
Wastopous <wastopous23@mail.ru>
2024-02-06 20:31:18 +0700

    fouth

--- Project10/ViewModels/MainWindow.axaml.cs
+++ Project10/ViewModels/MainWindow.axaml.cs
@@ -1,7 +1,11 @@
-﻿using Avalonia;
+﻿using System.Collections.ObjectModel;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using DynamicData;
+using MySql.Data.MySqlClient;
+using Project10.Models;
 
 namespace Project10.ViewModels;
 
@@ -15,12 +19,43 @@ public partial class MainWindow : Window
         
     }
 
-
+    private DataBase _dataBase = new DataBase();
+    private ObservableCollection<Good> _goods;
+    
+    private string s = "select goodid, goodname, gooddesc, goodcount, goodprice, providername, providername, category product" +
+                       " join producer p on p.producer_id = product.producer" +
+                       " join provider pp on product.provider = s.provider" +
+                       " join category pc on product.category = pc.categoryid";
 
     private void QuitButton_OnClick(object? sender, RoutedEventArgs e)
     {
         this.Close();
         LoginWindow loginWindow = new LoginWindow();
-        loginWindow.Show();
+        Show(s);
     }
+
+    public void Show(string _s)
+    {
+        _dataBase.openConnection();
+        MySqlCommand c = new MySqlCommand(_s, _dataBase.GetConnection());
+        MySqlDataReader r = c.ExecuteReader();
+        while (r.Read() && r.HasRows)
+        {
+            var curGood = new Good()
+            {
+            GoodID = r.GetInt32("GoodID"),
+            GoodName = r.GetString("GoodName"),
+            GoodDesc = r.GetString("GoodDesc"),
+            GoodCount = r.GetInt32("GoodCount"),
+            GoodPrice = r.GetDecimal("GoodPrice"),
+            Provider = r.GetString("ProviderName"),
+            Producer = r.GetString("ProducerName"),
+            Category = r.GetString("CategoryName")
+            };
+            _goods.Add(curGood);
+        }
+        _dataBase.CloseConnection();
+        GoodListBox.ItemsSource = _goods;
+    }
+    
 }
